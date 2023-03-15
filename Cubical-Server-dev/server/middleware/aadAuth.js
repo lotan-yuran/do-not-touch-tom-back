a1703afe8e218da002da7c6ae8c911b0d322996a
lotan yuran <lotan759@gmail.com>
2023-03-15 19:25:49 +0200

    fix

--- Cubical-Server-dev/server/middleware/aadAuth.js
+++ Cubical-Server-dev/server/middleware/aadAuth.js
@@ -3,11 +3,11 @@ const responseHandler = require("../utilities").responseHandler;
 module.exports = (req, res, next) => {
   if (req.isAuthenticated()) {
     // Todo: return it back
-    req.body.userId = req.user;
-    req.query.userId = req.user;
+    // req.body.userId = req.user;
+    // req.query.userId = req.user;
 
-    // req.body.userId = "322592973";
-    // req.query.userId = "322592973";
+    req.body.userId = "322592973";
+    req.query.userId = "322592973";
     return next();
   }
   return responseHandler.unauthorized(res);
